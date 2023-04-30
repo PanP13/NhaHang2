2a18166a8a6a22c9077f76452edfec4b7856ff63
PanP13 <panpanpanp13@gmail.com>
2023-05-01 03:19:00 +0700

    Update getTKbyID

--- src/DAO/TaiKhoan_DAO.java
+++ src/DAO/TaiKhoan_DAO.java
@@ -6,113 +6,110 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
-
 public class TaiKhoan_DAO {
-    public List<TaiKhoan> getAllTK(){
-        List <TaiKhoan> tks = new ArrayList<TaiKhoan>();
-        
+
+    public List<TaiKhoan> getAllTK() {
+        List<TaiKhoan> tks = new ArrayList<TaiKhoan>();
+
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM TaiKhoan";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 TaiKhoan tk = new TaiKhoan();
-                
+
                 tk.setIdTK(rs.getInt("IDTK"));
                 tk.setUsername(rs.getString("TenDN"));
                 tk.setPassword(rs.getString("MatKhau"));
                 tk.setLoaiTK(rs.getInt("MaLTK"));
                 tk.setMaTK(rs.getString("MaTK"));
-                
+
                 tks.add(tk);
             }
             conn.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return tks;
     }
-    
-    public TaiKhoan getTaiKhoanbyID(String id){
+
+    public TaiKhoan getTaiKhoanbyID(String id) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM TAIKHOAN WHERE id=?";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
-            
-            TaiKhoan tk = new TaiKhoan();
-            tk.setIdTK(rs.getInt("IDTK"));
-            tk.setUsername(rs.getString("TenDN"));
-            tk.setPassword(rs.getString("MatKhau"));
-            tk.setLoaiTK(rs.getInt("MaLTK"));
-            tk.setMaTK(rs.getString("MaTK"));
-            
-            conn.close();
-            return tk;
-        }
-        catch(SQLException e){
+
+            while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan();
+                tk.setIdTK(rs.getInt("IDTK"));
+                tk.setUsername(rs.getString("TenDN"));
+                tk.setPassword(rs.getString("MatKhau"));
+                tk.setLoaiTK(rs.getInt("MaLTK"));
+                tk.setMaTK(rs.getString("MaTK"));
+
+                conn.close();
+                return tk;
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
-    
-    public void addTK(TaiKhoan tk){
+
+    public void addTK(TaiKhoan tk) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "INSERT INTO TAIKHOAN(TENDN, MATKHAU, MALTK, MATK) VALUES (?, ?, ?, ?)";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, tk.getUsername());
             ps.setString(2, tk.getPassword());
             ps.setInt(3, tk.getLoaiTK());
             ps.setString(4, tk.getMaTK());
-            
+
             int rs = ps.executeUpdate();
             conn.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
-    public void updateTK(TaiKhoan tk){
+
+    public void updateTK(TaiKhoan tk) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "UPDATE TAIKHOAN SET TENDN=?, MATKHAU=?, MALTK=?, MATK=? WHERE IDTK=?";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, tk.getUsername());
             ps.setString(2, tk.getPassword());
             ps.setInt(3, tk.getLoaiTK());
             ps.setString(4, tk.getMaTK());
             ps.setInt(5, tk.getIdTK());
-            
+
             int rs = ps.executeUpdate();
             conn.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
-    public void deleteTK(String id){
+
+    public void deleteTK(String id) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "DELETE FROM TAIKHOAN WHERE IDTK=?";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, id);
-            
+
             int rs = ps.executeUpdate();
             conn.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
