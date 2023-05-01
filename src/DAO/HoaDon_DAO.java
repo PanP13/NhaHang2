d1e59f93aeb945513463b37a2e4f1711ba5a0dd0
PanP13 <panpanpanp13@gmail.com>
2023-05-02 03:11:25 +0700

    Update getHDbyID

--- src/DAO/HoaDon_DAO.java
+++ src/DAO/HoaDon_DAO.java
@@ -7,53 +7,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HoaDon_DAO {
-    public List<HoaDon> getAllHD(){
+
+    public List<HoaDon> getAllHD() {
         List<HoaDon> hds = new ArrayList<HoaDon>();
-        
+
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM HOADON";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 HoaDon hd = new HoaDon();
-                
+
                 hd.setMaHD(rs.getString("MaHD"));
                 hd.setMaKH(rs.getString("MaKH"));
                 hd.setMaNV(rs.getString("MaNV"));
                 hd.setMaBan(rs.getString("MaBan"));
-                
+
                 hds.add(hd);
             }
             conn.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return hds;
     }
-    
-    public HoaDon getHDbyID(String id){
+
+    public HoaDon getHDbyID(String id) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM HOADON WHERE MAHD=?";
-        
-        try{
+
+        try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
-            
-            HoaDon hd = new HoaDon();    
-            hd.setMaHD(rs.getString("MaHD"));
-            hd.setMaKH(rs.getString("MaKH"));
-            hd.setMaNV(rs.getString("MaNV"));
-            hd.setMaBan(rs.getString("MaBan"));
-            
-            conn.close();
-            return hd;
-        }
-        catch(SQLException e){
+
+            while (rs.next()) {
+                HoaDon hd = new HoaDon();
+                hd.setMaHD(rs.getString("MaHD"));
+                hd.setMaKH(rs.getString("MaKH"));
+                hd.setMaNV(rs.getString("MaNV"));
+                hd.setMaBan(rs.getString("MaBan"));
+
+                conn.close();
+                return hd;
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
