81f5ab585fe01ea6c006dd0cf8e356006c3d13c4
PanP13 <panpanpanp13@gmail.com>
2023-05-04 14:40:32 +0700

    update: change maLSP from String to int

--- src/DAO/LoaiSP_DAO.java
+++ src/DAO/LoaiSP_DAO.java
@@ -9,7 +9,7 @@ import java.sql.*;
 public class LoaiSP_DAO {
 
     public List<LoaiSP> getAllLSP() {
-        List<LoaiSP> ls = new ArrayList<LoaiSP>();
+        List<LoaiSP> ls = new ArrayList<>();
 
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM LOAISP";
@@ -21,7 +21,7 @@ public class LoaiSP_DAO {
             while (rs.next()) {
                 LoaiSP l = new LoaiSP();
 
-                l.setMaLSP(rs.getString("MaLSP"));
+                l.setMaLSP(rs.getInt("MaLSP"));
                 l.setTenLSP(rs.getString("TenLSP"));
 
                 ls.add(l);
@@ -35,17 +35,17 @@ public class LoaiSP_DAO {
         return ls;
     }
 
-    public LoaiSP getLSPbyID(String id) {
+    public LoaiSP getLSPbyID(int id) {
         Connection conn = JDBCConnection.getJDBCConnection();
         String sql = "SELECT * FROM LOAISP WHERE MALSP=?";
 
         try {
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 LoaiSP l = new LoaiSP();
-                l.setMaLSP(rs.getString("MaLSP"));
+                l.setMaLSP(rs.getInt("MaLSP"));
                 l.setTenLSP(rs.getString("TenLSP"));
 
                 conn.close();
