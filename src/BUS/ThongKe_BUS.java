2c6adc7d1b8f93dd105a3dd81d5b6ecd29202ee6
PanP13 <panpanpanp13@gmail.com>
2023-05-05 16:36:32 +0700

    update: thongke

---
+++ src/BUS/ThongKe_BUS.java
@@ -0,0 +1,20 @@
+package BUS;
+
+import DAO.ThongKe_DAO;
+import java.util.List;
+
+public class ThongKe_BUS {
+    private ThongKe_DAO daoTKE;
+    
+    public ThongKe_BUS(){
+        daoTKE = new ThongKe_DAO();
+    }
+    
+    public String getTK(){
+        return daoTKE.getTK();
+    }
+    
+    public List<String> getTKHDbyKH(){
+        return daoTKE.getTKHDbyKH();
+    }
+}
